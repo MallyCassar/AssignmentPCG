422f55245bb2e22444ab46eb97500b87c2b785b8
Malcolm Cassar <a100938@mcast.org.mt>
2017-12-14 15:05:09 +0100

    Added Scenes 'Level1, Level2 and Level3', each scene contrains 2 paddles controlled by seperate players, a ball, 2 goal posts & boundaries, added Sprites for all of the objects, added a script to both paddles and the ball, added a main menu scene.

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    public GameObject P1;
+
+    // Update is called once per frame
+    void Update () {
+
+        float mouseY = (Input.mousePosition.y / Screen.height * 14f) - 7f;
+        mouseY = Mathf.Clamp(mouseY, -4f, 4f);
+
+        //Vector3 Saves any point in (x,y,z)
+        //keeps the y position fixed
+        Vector3 paddlePosition = new Vector3(-7.24f, mouseY, 0);
+
+        //sets the position of the paddle (this) to the paddlePosition
+        this.transform.position = paddlePosition;
+
+    }
+}
---
+++ Assets/Scripts/Paddle2.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle2 : MonoBehaviour {
+
+
+   // public GameObject Right;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (this.transform.position.y <= -4f || this.transform.position.y >= 4f)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0f, 0f, 0f);
+        }
+
+        
+
+        // P2 Up
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Player 2 is pressing Up Arrow");
+            Mathf.Clamp(GetComponent<Transform>().position.y, -4f, 4f);
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0f, 4f, 0f);
+        }
+
+        //P2 Down
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("Player 2 is pressing Down Arrow");
+            //Mathf.Clamp(0, -4f, 4F);
+            //Mathf.Clamp(Right.GetComponent<Transform>().position.y, -4f, 4f);
+            GetComponent<Rigidbody2D>().velocity = new Vector3(0f, -4f, 0f);
+            
+        }
+
+        
+    }
+}
