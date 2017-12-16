5e197a04d5f8cdc9583011dede572e34a761192c
Malcolm Cassar <32704501+MallyCassar@users.noreply.github.com>
2017-12-16 09:45:56 +0100

    Updated The Score, Not fully complete.. Need to add total score Done using player prefs and some research.
    Updated The Score, Not fully complete.. Need to add total score Done using player prefs and some research.

---
+++ Assets/Scripts/menuscript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuscript : MonoBehaviour
+{
+    public void startgame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+
+    public void quitgame()
+    {
+        Application.Quit();
+    }
+}
